2605f31afcd1805d43b9d517082e47f501ecb634
AndreaLavezzi <72445472+AndreaLavezzi@users.noreply.github.com>
2021-09-30 09:08:11 +0200

    Esercizi 1 e 2
    Esercizio 1: Creazione di una classe con dei membri (attributi e metodi) e di un'istanza della classe.
    Esercizio 2: Creazione di una classe con dei membri (attributi e metodi) e di un'istanza della classe in base a ciò he viene inserito da tastiera.

---
+++ programmazioneAdOggetti1/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Tramite la programmazione ad oggetti scrivere il codice di un programma
+ *che dopo aver costruito la classe Calciatore visualizzi in output: il 
+ *nome del calciatore, il ruolo, la squadra ed i gol segnati (i dati vengono
+ *assegnati all'interno del codice).*/
+
+using System; /*istruzione per l'utilizzo della libreria system che permette di usare
+               * la classe console, che contiene i metodi Write, ReadLine e altri*/
+
+namespace programmazioneAdOggetti1
+{
+    class Calciatore //definizione della classe (pag. 20)
+    {
+        //attributi
+        string nome, squadra, ruolo;
+        int golSegnati;
+
+        //metodo di default: costruttore
+        public Calciatore(string nome, string squadra, string ruolo)
+        {
+            this.nome = nome;
+            this.squadra = squadra;
+            this.ruolo = ruolo;
+            golSegnati = 0;
+        }
+
+        //metodi
+        public void aggiornaGolSegnati(int gol)
+        {
+            golSegnati += gol;
+        }
+
+        public void visualizzaGol()
+        {
+            Console.WriteLine("{0} - {3} - {2} - gol segnati: {1}", nome, golSegnati, ruolo, squadra);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calciatore c = new Calciatore("Giovanni Marchetto", "A.C. Cantonazzo", "Raccattapalle");
+            c.aggiornaGolSegnati(69420);
+            c.visualizzaGol();
+        }
+    }
+}
---
+++ programmazioneAdOggetti2/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Tramite la programmazione ad oggetti scrivere il codice di un programma
+ *che dopo aver costruito la classe Calciatore visualizzi in output: il 
+ *nome del calciatore, il ruolo, la squadra ed i gol segnati (i dati vengono
+ *assegnati all'interno del codice).*/
+
+using System; /*istruzione per l'utilizzo della libreria system che permette di usare
+               * la classe console, che contiene i metodi Write, ReadLine e altri*/
+
+namespace programmazioneAdOggetti1
+{
+    class Calciatore //definizione della classe (pag. 20)
+    {
+        //attributi
+        string nome, squadra, ruolo;
+        int golSegnati;
+
+        //metodo di default: costruttore
+        public Calciatore(string nome, string squadra, string ruolo)
+        {
+            this.nome = nome;
+            this.squadra = squadra;
+            this.ruolo = ruolo;
+            golSegnati = 0;
+        }
+
+        //metodi
+        public void aggiornaGolSegnati(int gol)
+        {
+            golSegnati += gol;
+        }
+
+        public void visualizzaGol()
+        {
+            Console.WriteLine("{0} - {1} - {2} - gol segnati: {3}", nome, squadra, ruolo, golSegnati);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, ruolo, squadra;
+            Console.Write("Inserire il nome del giocatore:\nRisposta: ");
+            nome = Console.ReadLine();
+            Console.Write("\nInserire la squadra del giocatore:\nRisposta: ");
+            squadra = Console.ReadLine();
+            Console.Write("\nInserire il ruolo del giocatore:\nRisposta: ");
+            ruolo = Console.ReadLine();
+            Calciatore c = new Calciatore(nome, squadra, ruolo);
+            Console.WriteLine("\nIl tuo calciatore:");
+            c.visualizzaGol();
+
+        }
+    }
+}
