a236336b474af584976d7eca210e6d96bd7c5918
AndreaLavezzi <72445472+AndreaLavezzi@users.noreply.github.com>
2021-10-01 09:27:07 +0200

    Esercizi 4 e 5
    Esercizio 4: tramite la programmazione ad oggetti scrivere un programma in C# che dopo aver letto in input il
    raggio di una circonferenza, calcoli e visualizzi in output la misura del diametro (2*r), della
    circonferenza (2*r*pi) e la sua area (r 2 *pi). (Utilizzare la funzione Math.PI).
    
    Esercizio 5: dopo aver acquisito da tastiera una serie di dati relativi alla misurazione della temperatura di una
    certa città, scrivere il codice di un programma(OOP) in C# che determini e visualizzi la temperatura
    più bassa e quella più alta.

---
+++ programmazioneAdOggetti4/Program.cs
@@ -0,0 +1,50 @@
+﻿//Autore: Lavezzi Andrea
+//Data: 01/10/2021
+/*Consegna: Tramite la programmazione ad oggetti scrivere un programma in C# che dopo aver letto in input il
+raggio di una circonferenza, calcoli e visualizzi in output la misura del diametro (2*r), della
+circonferenza (2*r*pi) e la sua area (r 2 *pi). (Utilizzare la funzione Math.PI).*/
+using System;
+
+namespace programmazioneAdOggetti4
+{
+    class Circonferenza //creo la classe Circonferenza
+    {
+        double raggio, diametro, circonferenza, area;
+        public Circonferenza(double raggio) //tramite il costruttore acquisisco il raggio e calcolo gli altri attributi
+        {
+            this.raggio = raggio;
+            this.diametro = raggio * 2;
+            this.circonferenza = 2 * raggio * Math.PI;
+            this.area = raggio * raggio * Math.PI;
+        }
+
+        public void MostraMisure() //mostro a schermo le misure della figura
+        {
+            Console.WriteLine("\nQueste sono le misure della figura: Raggio: {0}, Diametro: {1}, Circonferenza: {2}, Area: {3}", raggio, diametro, circonferenza, area);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raggio;
+            string ricreare = "y"; //stringa che permette all'utente di creare varie circonferenze senza dover chiudere e riaprire il programma
+            while(ricreare == "y")
+            {
+                Console.Write("Inserire la misura del raggio:\nRisposta: "); //chiedo la misura del raggio
+                raggio = Double.Parse(Console.ReadLine()); //ottengo la misura del raggio da tastiera
+                while(raggio < 0)
+                {
+                    Console.Write("\nUna misura non può essere minore di 0. Reinserire la misura del raggio: ");
+                    raggio = Double.Parse(Console.ReadLine());
+                }
+                Circonferenza c = new Circonferenza(raggio); //creo un'istanza della classe Circonferenza passandole il raggio
+                c.MostraMisure(); //mostro a schermo le misure
+                //chiedo all'utente se vuole continuare
+                Console.WriteLine("\nVuoi creare una nuova circonferenza? [Scrivere 'y' per continuare, altro per terminare il programma.]\n");
+                ricreare = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ programmazioneAdOggetti5/Program.cs
@@ -0,0 +1,49 @@
+﻿//Autore: Lavezzi Andrea
+//Data: 01/20/2021
+/*Consegna: Dopo aver acquisito da tastiera una serie di dati relativi alla misurazione della temperatura di una
+certa città, scrivere il codice di un programma(OOP) in C# che determini e visualizzi la temperatura
+più bassa e quella più alta. */
+using System;
+
+namespace programmazioneAdOggetti5
+{
+    class Città
+    {
+        string nome;
+        double[] temperature = new double[0]; //creo un array vuoto
+        public Città(string nome) //tramite il costruttore chiedo il nome della città
+        {
+            this.nome = nome;
+        }
+
+        public void AggiungiTemperatura() //aggiungo temperature all'array in base a quante ne vuole l'utente, affinchè egli risponda con "y"
+        {
+            string redo = "y";
+            while(redo == "y")
+            {
+                Console.Write("\nInserire la temperatura registrata: ");
+                Array.Resize(ref temperature, temperature.Length + 1);
+                temperature[temperature.Length - 1] = double.Parse(Console.ReadLine());
+                Console.Write("\nVuoi inserire una nuova temperatura? Inserire 'y' per continuare, altro per terminare l'inserimento.\n");
+                redo = Console.ReadLine();
+            }
+        }
+
+        public void VisualizzaTemperature() //comunico il nome della città e il valore delle temperature
+        {
+            Array.Sort(temperature);
+            Console.WriteLine("TEMPERATURE DI {0} \nTemperatura Massima: {1}, \nTemperatura Minima: {2}", nome.ToUpper(), temperature[temperature.Length - 1], temperature[0]);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserire il nome della città: "); //chiedo il nome della città
+            string nome = Console.ReadLine();
+            Città città = new Città(nome); //creo un'istanza della classe città col nome inserito dall'utente
+            città.AggiungiTemperatura(); //aggiungo le temperature
+            città.VisualizzaTemperature(); //visualizzo il nome della città e la temperatura massima e minima
+        }
+    }
+}
